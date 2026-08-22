fe24b6e1e64fadb8931d73ac9f87f2becfaa322b
swelehwaseem-pixel <sweleh.waseem@gmail.com>
2026-08-22 09:30:03 +0300

    Update script_manager.py

--- app/routers/script_manager.py
+++ app/routers/script_manager.py
@@ -8,7 +8,7 @@ from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, WebSock
 from fastapi.responses import FileResponse, JSONResponse
 from pydantic import BaseModel, Field
 from app.auth import get_current_user
-from app.database import User
+from app.models.user import User
 import stat
 import mimetypes
 
